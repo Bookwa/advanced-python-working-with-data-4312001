a641ddffee22e5841b6cd29c7df8b913b03b58e4
LucijaB <80901280+Bookwa@users.noreply.github.com>
2023-07-07 08:45:32 +0000

    modified:   Start/Ch_1/challenge_start.py 	modified:   Start/Ch_1/filtering.py 	modified:   Start/Ch_1/minmax.py 	modified:   Start/Ch_1/sorting.py 	modified:   Start/Ch_1/transform.py 	modified:   Start/Ch_1/utility.py 	modified:   Start/Ch_2/counter.py

--- Start/Ch_1/challenge_start.py
+++ Start/Ch_1/challenge_start.py
@@ -5,12 +5,28 @@ import json
 
 
 # for this challenge, we're going to summarize the earthquake data as follows:
-# 1: How many quakes are there in total?
+
 # 2: How many quakes were felt by at least 100 people?
 # 3: Print the name of the place whose quake was felt by the most people, with the # of reports
 # 4: Print the top 10 most significant events, with the significance value of each
 
 # open the data file and load the JSON
-with open("../../30DayQuakes.json", "r") as datafile:
+with open("/workspaces/advanced-python-working-with-data-4312001/30DayQuakes.json", "r") as datafile:
     data = json.load(datafile)
+
+# 1: How many quakes are there in total?
+def isAQuake(q):
+    if q["properties"]["type"] == "earthquake":
+        return True
+    return False
+
+events = list(filter(isAQuake, data["features"]))
+print(f"Total number of Quake events: {len(events)}")
+for i in range(0, 10):
+    print(events[i]["properties"]["type"])
+
+print(sum(quake["properties"]["felt"] is not None and quake["properties"]["felt"] >= 100
+          for quake in data["features"]))
+
+
     
--- Start/Ch_2/counter.py
+++ Start/Ch_2/counter.py
@@ -12,15 +12,43 @@ class2 = ["Bill", "Barry", "Cindy", "Debbie", "Frank",
           "Gabby", "Kelly", "James", "Joe", "Sam", "Tara", "Ziggy"]
 
 # TODO: Create a Counter for class1 and class2
+words1 = Counter(c for clist in class1 for c in clist)
+print(words1)
+words2 = Counter(d for dlist in class2 for d in dlist)
+print(words2)
 
 # TODO: How many students in class 1 named James?
-
+james = class1.count("James")
+print(james)
 # TODO: How many students are in class 1?
+num_students = len(class1)
+print("The number of students in class 1 is:", num_students)
 
 # TODO: Combine the two classes
+classes = class1 + class2
+print(classes)
 
 # TODO: What's the most common name in the two classes?
+name_counts = Counter(classes)
+most_common_name = name_counts.most_common(1)[0][0]
+
+print("The most common name in the class is:", most_common_name)
 
 # TODO: Separate the classes again
+combined_class = ['Bob', 'James', 'Chad', 'Darcy', 'Penny', 'Hannah', 'Kevin', 'James', 'Melanie', 'Becky', 'Steve',
+                  'Frank', 'Bill', 'Barry', 'Cindy', 'Debbie', 'Frank', 'Gabby', 'Kelly', 'James', 'Joe', 'Sam', 'Tara', 'Ziggy']
+max_students_per_class = 12
+
+# Split the combined class into separate classes
+num_classes = len(combined_class) // max_students_per_class
+if len(combined_class) % max_students_per_class != 0:
+    num_classes += 1
+
+separated_classes = [combined_class[i * max_students_per_class:(
+    i + 1) * max_students_per_class] for i in range(num_classes)]
+
+# Print the separated classes
+for i, class_names in enumerate(separated_classes):
+    print(f"Class {i+1}: {class_names}")
 
 # TODO: What's common between the two classes?
